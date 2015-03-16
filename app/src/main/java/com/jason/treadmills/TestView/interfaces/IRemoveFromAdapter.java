2700c9764042c0f0d7329f9e447220ea0e454d25
qiu.shiping <qiusip@gmail.com>
2015-03-16 18:03:53 +0800

    add test coverflowaty

---
+++ app/src/main/java/com/jason/treadmills/TestView/interfaces/IRemoveFromAdapter.java
@@ -0,0 +1,6 @@
+package com.jason.treadmills.TestView.interfaces;
+
+
+public interface IRemoveFromAdapter{
+	void removeItemFromAdapter(int position);
+}
---
+++ app/src/main/java/com/jason/treadmills/ui/activity/TCoverFlowActivity.java
@@ -0,0 +1,108 @@
+package com.jason.treadmills.ui.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import com.jason.treadmills.R;
+import com.jason.treadmills.TestView.FeatureCoverFlow;
+import com.jason.treadmills.model.GameEntity;
+import com.jason.treadmills.ui.adapter.CoverFlowAdapter;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class TCoverFlowActivity extends ActionBarActivity {
+
+    @InjectView(R.id.ViewTCoverFlowAty)
+    FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    @InjectView(R.id.title)
+    TextSwitcher mTitle;
+    private ArrayList<GameEntity> mData = new ArrayList<>(0);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tcover_flow);
+        ButterKnife.inject(this);
+        mData.add(new GameEntity(R.drawable.image_1, R.string.title1));
+        mData.add(new GameEntity(R.drawable.image_2, R.string.title2));
+        mData.add(new GameEntity(R.drawable.image_3, R.string.title3));
+        mData.add(new GameEntity(R.drawable.image_4, R.string.title4));
+
+        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(TCoverFlowActivity.this);
+                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+                return textView;
+            }
+        });
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_top);
+        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_bottom);
+        mTitle.setInAnimation(in);
+        mTitle.setOutAnimation(out);
+        mCoverFlow.setSpacing(0.7f);
+        mAdapter = new CoverFlowAdapter(this);
+        mAdapter.setData(mData);
+
+        mCoverFlow.setAdapter(mAdapter);
+
+        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(TCoverFlowActivity.this,
+                        getResources().getString(mData.get(position).titleResId),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                mTitle.setText(getResources().getString(mData.get(position).titleResId));
+            }
+
+            @Override
+            public void onScrolling() {
+                mTitle.setText("");
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tcover_flow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
